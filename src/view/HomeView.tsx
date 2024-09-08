1f7127534ff3fd038b4c96f3e2cff73862bcc354
meskatabusayed <meskatabusayed@gmail.com>
2024-09-08 22:13:39 +0530

    handled some issue on review section

--- src/view/HomeView.tsx
+++ src/view/HomeView.tsx
@@ -1,8 +1,8 @@
 // import FeaturedServices from "@/components/home/FeaturedServices";
 // import HeroSection from "@/components/home/HeroSection";
-// import Reviews from "@/components/home/Reviews";
+import Reviews from "../components/home/Reviews";
 
-import { MdReviews } from "react-icons/md";
+
 import FeaturedServices from "../components/home/FeaturedServices";
 import HeroSection from "../components/home/HeroSection";
 
@@ -11,7 +11,8 @@ const HomeView = () => {
     <>
       <HeroSection />
       <FeaturedServices />
-      <MdReviews />
+      <Reviews />
+      
     </>
   );
 };
